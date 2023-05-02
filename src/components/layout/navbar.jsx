6ebfcd1da7cd2c5979b0e5ad1c07b0e97ae369d5
Muhammad Talha <talhamasood0000@gmail.com>
2023-05-02 18:46:45 +0500

    added page component

---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>This is navbar</div>
+  )
+}
+
+export default navbar
---
+++ src/components/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>This is page</div>
+  )
+}
+
+export default page
