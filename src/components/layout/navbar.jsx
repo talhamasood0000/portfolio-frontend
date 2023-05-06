be9c7a2a439f9f421822a4e8d9d1d0814fd08c81
Muhammad Talha <talhamasood0000@gmail.com>
2023-05-06 17:47:29 +0500

    Add logo for website

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Button from '@mui/material/Button'
-import { ShoppingCartCheckout } from '@mui/icons-material';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { ShoppingCartCheckout } from "@mui/icons-material";
+import Image from "next/image";
+import logo from "../../media/test.svg";
+import logo1 from "../../media/download.svg";
 const Navbar = () => {
   return (
-    <AppBar sx={{background: '#063970'}}>
-    <Toolbar>
-      <ShoppingCartCheckout />
-      <Tabs sx={{ marginLeft: "auto" }} textColor="inherit">
-        <Tab label="First" />
-        <Tab label="Second" />
-        <Tab label="Third" />
-      </Tabs>
-      <Button variant='contained'>Resume</Button>
-    </Toolbar>
-    
-  </AppBar>
-  )
-}
+    <>
+      <AppBar sx={{ background: "#063970" }}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Image src={logo} alt="Logo" width={70} />
+          </IconButton>
+          <Tabs sx={{ marginLeft: "auto" }} textColor="eeeee4">
+            <Tab label="First" />
+            <Tab label="Second" />
+            <Tab label="Third" />
+          </Tabs>
+          <Button variant="contained">Resume</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Navbar;
