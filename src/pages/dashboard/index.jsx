5b048eb36792c9efeba3a9b21d3ec9d52bae8a32
Muhammad Talha <talhamasood0000@gmail.com>
2023-05-21 22:52:11 +0500

    Refactoring

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,19 +1,26 @@
 import Navbar from "@/components/layout/navbar";
-import Banner from "@/components/banner"; 
+import Banner from "@/components/banner";
+import Blogs from "@/components/blogs";
+import Footer from "@/components/layout/footer";
 import Projects from "@/components/projects";
-import { Box, Stack } from '@mui/material';
-import { Grid, Paper } from '@mui/material';
+import { Stack } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
-const  Dashboard= () => {
-    return ( 
-        <>
-            <Stack>
-                <Navbar />
-                <Banner />  
-                <Projects />  
-            </Stack>
-        </>
-    );
-}
- 
+const Dashboard = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Stack>
+          <Navbar />
+          <Banner />
+          {/* <Projects /> */}
+          <Blogs />
+          <Footer />
+        </Stack>
+      </ThemeProvider>
+    </>
+  );
+};
+
 export default Dashboard;
