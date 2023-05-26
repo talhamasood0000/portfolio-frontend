7d18c1b147934ee8c058ee98cebe7942e3b168a4
Muhammad Talha <talhamasood0000@gmail.com>
2023-05-26 22:56:54 +0500

    Refactoring

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -1,13 +1,10 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
